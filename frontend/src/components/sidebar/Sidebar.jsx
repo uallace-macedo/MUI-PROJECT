a09f33325ec901df162e4c55bb1b1e05275f9b98
Uallace Macedo <uallace.macedo@outlook.com>
2025-04-09 19:01:54 -0300

    feat: add navbar button to mobile mode

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
-import { AccountBox, Group, Home, Article, Person,  Settings, Store, ModeNight } from '@mui/icons-material';
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, styled, Switch } from '@mui/material';
+import { AccountBox, Group, Home, Article, Person,  Settings, Store } from '@mui/icons-material';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material';
 import { useThemesContext } from '../../theme/ThemesContextProvider';
 
 
@@ -7,7 +7,7 @@ const Sidebar = () => {
   const { toggleThemeMode } = useThemesContext();
 
   return (
-    <Box  flex={1} p={2} sx={{display: {xs: 'none', sm: 'block'}, minWidth: '170px'}}>
+    <Box flex={1} p={2} sx={{display: {xs: 'none', sm: 'block'}, minWidth: '170px'}}>
       <Box position='fixed'>
         <List>
           <ListItem disablePadding>
